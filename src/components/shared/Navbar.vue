1f2c2906bfb7a25ee63cc8f8573bbbc23f2eee34
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-21 20:31:54 -0400

    add navbar items

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,7 +1,15 @@
 <template>
-  <nav>A</nav>
+  <v-app-bar>
+    <template v-slot:append>
+      <v-btn class="my-button" icon="mdi-github" size="large"></v-btn>
+      <v-btn icon="mdi-linkedin" size="large"></v-btn>
+      <v-btn icon="mdi-email" size="large"></v-btn>
+
+    </template>
+  </v-app-bar>
 </template>
 
 <script setup>
   
 </script>
+
