5fc7e856efc5ab42ae649df69071dd6cbd9e6fb3
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-24 16:20:44 -0400

    add form component to register and login

---
+++ src/modules/auth/components/Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="mx-auto mt-5" max-width="450">
+    <v-card-text>
+      <slot name="content"></slot>
+    </v-card-text>
+
+    <v-card-actions class="pt-0">
+     <slot name="buttons"></slot>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+.v-card {
+  background-color: #101314;
+  color: white;
+}
+</style>
