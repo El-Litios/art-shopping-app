015b83a27a6b3fa6d1391ea7ea6a1f559ad4b8ea
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-24 16:20:32 -0400

    add routes to redirect

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app-bar>
     <template v-slot:append>
-      <v-btn class="text-h5" >Registro <v-icon class="mdi mdi-account-plus" size="x-small"></v-icon> </v-btn>
-      <v-btn class="text-h5" >Ingresar <v-icon class="mdi mdi-login" size="x-small"></v-icon> </v-btn>
+      <v-btn class="text-h5" :to="{name: 'Register'}" >Registro <v-icon class="mdi mdi-account-plus" size="x-small"></v-icon> </v-btn>
+      <v-btn class="text-h5" :to="{name: 'Login'}" >Ingresar <v-icon class="mdi mdi-login" size="x-small"></v-icon> </v-btn>
     </template>
   </v-app-bar>
 </template>
