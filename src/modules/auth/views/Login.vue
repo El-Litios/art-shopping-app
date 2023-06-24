9ba0b549e7970eadda22319a611e8020a6855e67
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-24 16:21:44 -0400

    import form component and slots

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,11 +1,35 @@
 <template>
-  <Navbar/>
+  <Navbar />
+  <h1 class="text-center text-h1 mt-2">Ingreso</h1>
+  <v-form>
+    <LoginForm>
+      <template v-slot:content>
+        <p class="text-h5 text--primary">Correo:</p>
+        <v-text-field v-model="user.email"></v-text-field>
+        <p class="text-h5 text--primary">Contraseña:</p>
+        <v-text-field v-model="user.pass"></v-text-field>
+      </template>
+      <template v-slot:buttons>
+        <v-btn class="auth-btn" @click="ver" block>Ingresar</v-btn>
+      </template>
+    </LoginForm>
+  </v-form>
 </template>
 
 <script setup>
-import Navbar from '@/components/shared/Navbar'
+import { ref } from "vue";
+import Navbar from "@/components/shared/Navbar";
+import LoginForm from "../components/Form";
+
+const user = ref({
+  username: "",
+  pass: "",
+});
 </script>
 
 <style>
-
+.auth-btn{
+    background-color: #0044D1;
+    font-size: large;
+}
 </style>
