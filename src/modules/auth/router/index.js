4210e099c0f94f79d9cc519217580126862ec5c9
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-24 02:05:41 -0400

    add auth routes module

---
+++ src/modules/auth/router/index.js
@@ -0,0 +1,16 @@
+export default {
+    name: 'auth',
+    component: () => import(/* webpackChunkName: 'auth layout' */ '@/modules/auth/layouts/AuthLayout.vue'),
+    children: [
+         {
+            path: '',
+            name: 'Login',
+            component: () => import(/* webpackChunkName: 'auth login'*/  '@/modules/auth/views/Login.vue'),
+        },/*
+        {
+            path: 'register',
+            name: 'Register',
+            component: () => import(/* webpackChunkName: 'auth register'  '@/modules/auth/views/Register.vue'),
+        } */
+    ]
+}
