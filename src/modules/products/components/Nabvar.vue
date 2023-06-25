a546b75099b9180adbb15ad89e6e56108bf134ea
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-25 02:15:20 -0400

    add Navbar component to products module

---
+++ src/modules/products/components/Nabvar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app-bar>
+      <template v-slot:append>
+        <p class="text-white text-h6"><b>Bienvenido: </b>Carlos</p>
+        <v-btn class="text-h5"> <v-icon class="mdi mdi-cart" size="x-small"></v-icon> (1)</v-btn>
+      </template>
+    </v-app-bar>
+  </template>
+  
+  <script setup>
+    
+  </script>
+  
+  <style lang="css">
+  </style>
+  
