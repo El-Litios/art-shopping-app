099baebd53410b323e3ce20b4a16cf0fb9ce7f23
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-26 19:53:37 -0400

    add info to cards

--- src/modules/products/components/Card.vue
+++ src/modules/products/components/Card.vue
@@ -1,37 +1,45 @@
 <template>
   <v-card class="mx-auto mt-5" max-width="344">
-    <v-card-text>
-      <div>Word of the Day</div>
-      <p class="text-h4 text--primary">el·ee·mos·y·nar·y</p>
-      <p>adjective</p>
-      <div class="text--primary">
-        relating to or dependent on charity; charitable.<br />
-        "an eleemosynary educational institution."
+    <v-img
+      class="mx-auto mt-4 pa-3 card-logo"
+      src="@/assets/images/logo.png"
+      width="90"
+      height="90"
+    ></v-img>
+    <v-card-text class="pt-0">
+      <p class="text-h4 text-center text--primary icon-label">
+        Ymagin<b>arte</b>
+      </p>
+      <div
+        class="text-subtitle-1 text-gray mt-2 text-center mr-2 ml-2 card-body"
+      >
+        <p>
+          Somos una tienda y equipo pequeño dedicados a fabricar ideas y
+          productos poco habituales.
+        </p>
+        <p>
+          Selecciona tu producto y envia una solicitud para contactarnos contigo
+          de manera personalizada.
+        </p>
+      </div>
+      <div class="mt-4 text-center">
+        <v-icon class="mdi mdi-map-marker"></v-icon>
+        <span class="text-subtitle-1">Curicó.</span>
+      </div>
+      <div
+        class="text-subtitle-1 text-gray mt-3 text-center mr-1 ml-1 card-footer"
+      >
+        <p class="text-h5">¿Tienes algún proyecto?</p>
+        <v-divider :thickness="3" class="border-opacity-25 ma-2"></v-divider>
+        <p>
+          Puedes contratarnos para un servicio especifico o algun proyecto que
+          quieras compartir con nosotros. Somos un equipo pequeño que cuenta con
+          profesionales en diseño.
+        </p>
+
+        <v-btn class="ma-2 text-subtitle-1">Correo Electrónico</v-btn>
       </div>
     </v-card-text>
-    <v-card-actions>
-      <v-btn variant="text" color="teal-accent-4" @click="reveal = true">
-        Learn More
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <v-card v-if="reveal" class="v-card--reveal" style="height: 100%">
-        <v-card-text class="pb-0">
-          <p class="text-h4 text--primary">Origin</p>
-          <p>
-            late 16th century (as a noun denoting a place where alms were
-            distributed): from medieval Latin eleemosynarius, from late Latin
-            eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’
-          </p>
-        </v-card-text>
-        <v-card-actions class="pt-0">
-          <v-btn variant="text" color="teal-accent-4" @click="reveal = false">
-            Close
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-expand-transition>
   </v-card>
 </template>
 
@@ -39,4 +47,33 @@
 </script>
 
 <style>
+.card-logo {
+  background-color: white !important;
+  border-radius: 100%;
+}
+
+.icon-label {
+  font-family: "Roboto" !important;
+  font-size: 25px !important;
+  color: #4c8ecc;
+}
+
+.icon-label b {
+  color: #a77308;
+}
+
+.card-body {
+  letter-spacing: 0.0206em !important;
+}
+.card-footer {
+  letter-spacing: 1.2px !important;
+  color: #a1caf0;
+  border: 1px solid #61a1dd;
+  border-radius: 20px;
+}
+.card-footer .v-btn{
+  background-color: transparent !important;
+  border: 1px solid #61a1dd;
+  color: #a1caf0;
+}
 </style>
