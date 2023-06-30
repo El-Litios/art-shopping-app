e66edeca83a8233fd4056ee649076baa38f7d246
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-30 00:20:20 -0400

    import carousel and link components

--- src/modules/products/views/Products.vue
+++ src/modules/products/views/Products.vue
@@ -1,14 +1,14 @@
 <template>
 <Navbar/>
 <v-row class="mt-3">
-    <v-col lg="3" md="12" sm="12" xs="1">
+    <v-col lg="3" md="12" sm="12" cols="12">
         <Card></Card>
     </v-col>
-    <v-col lg="6" md="12" sm="12" xs="1">
-        <Card></Card>
+    <v-col lg="6" md="12" sm="12" cols="12">
+        <Carousel></Carousel>
     </v-col>
-    <v-col lg="3" md="12" sm="12" xs="1">
-        <Card></Card>
+    <v-col lg="3" md="12" sm="12" cols="12">
+        <Links></Links>
     </v-col>
 </v-row>
 </template>
@@ -16,6 +16,8 @@
 <script setup>
 import Navbar from '../components/Nabvar.vue'
 import Card from '../components/Card.vue'
+import Carousel from '../components/Carousel.vue'
+import Links from '../components/Links.vue'
 </script>
 
 <style>
