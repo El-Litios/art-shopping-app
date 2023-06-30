63b787ac5d24f538a6cfefee0561cb0295b7276a
El-Litios <carlosguzmanrojas00@gmail.com>
2023-06-30 00:20:00 -0400

    add Links component

---
+++ src/modules/products/components/Links.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row class="mx-auto mt-2 text-center" max-width="344">
+    <v-col cols="12">
+        <v-btn class="pa-0 mx-auto" height="127"> 
+            <v-img src="@/assets/images/links/Instagram.png" max-width="300">
+            </v-img>
+        </v-btn>
+    </v-col>
+    <v-col cols="12">
+        <v-btn class="pa-0 mx-auto" height="127"> 
+            <v-img src="@/assets/images/links/Behance.png" max-width="300">
+            </v-img>
+        </v-btn>
+    </v-col>
+    <v-col cols="12">
+        <v-btn class="pa-0 mx-auto" height="127"> 
+            <v-img src="@/assets/images/links/TikTok.png" max-width="300">
+            </v-img>
+        </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
