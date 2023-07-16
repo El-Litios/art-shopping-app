29c5da5107c92b6b923496b5159cdb05f499abd8
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-16 18:47:16 -0400

    adding data

--- src/api/index.js
+++ src/api/index.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "#",
-  authDomain: "#",
-  projectId: "#",
-  storageBucket: "#",
-  messagingSenderId: "#",
-  appId: "#"
+  apiKey: "AIzaSyB91XWCMVkkn4ack1lmvY2WgtORFMjZuTA",
+  authDomain: "ymaginarte-caf04.firebaseapp.com",
+  projectId: "ymaginarte-caf04",
+  storageBucket: "ymaginarte-caf04.appspot.com",
+  messagingSenderId: "637037918163",
+  appId: "1:637037918163:web:e24965c0a2e31ee830e126"
 };
 
 // Initialize Firebase
