8261723e288ede40d88b4da449ffebbdda25b1c4
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-19 02:14:25 -0400

    import products store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
 import AuthModule from '@/modules/auth/store'
+import ProductModule from '@/modules/products/store'
 // Create a new store instance.
 const store = createStore({
-  modules: [AuthModule]
+  modules: [AuthModule, ProductModule]
 })
 
 const app = createApp({ /* your root component */ })
