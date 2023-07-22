8447eb5b238e90ec3fb4c5a3178c0d26d70b2049
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-22 15:03:14 -0400

    adding function to getProducts from store

--- src/modules/products/composables/useProducts.js
+++ src/modules/products/composables/useProducts.js
@@ -6,10 +6,15 @@ const useProducts = () => {
   const getCategories = async() => {
     await  store.dispatch("products/getCategoriesList")
   }
+  const getProducts = async() => {
+    await  store.dispatch("products/getProductsList")
+  }
 
   return {
     getCategories,
+    getProducts,
     categories: computed(() => store.getters["products/categoriesInfo"]),
+    products: computed(() => store.getters["products/productsInfo"]),
   }
 };
 
