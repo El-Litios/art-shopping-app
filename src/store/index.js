3b5b3c2ce8a16a3f0f663f128abe15d692b703ff
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-23 20:10:25 -0400

    import administration store module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@
 import { createStore } from 'vuex'
 import auth from '@/modules/auth/store/index'
 import products from '@/modules/products/store/index'
+import administration from '@/modules/administration/store/index'
 // Create a new store instance.
 const store = createStore({
   modules: {
     auth,
-    products
+    products,
+    administration
   }
 })
 
