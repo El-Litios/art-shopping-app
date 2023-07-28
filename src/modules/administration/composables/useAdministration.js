93c6f9f48c2e0488a2d72619ea07ab672ce87b26
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-28 01:30:15 -0400

    add administration composable

---
+++ src/modules/administration/composables/useAdministration.js
@@ -0,0 +1,19 @@
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
+
+const useAdministration = () => {
+  const store = useStore();
+  //const searchTerm = ref('');
+
+  const getUsers = async() => {
+    await  store.dispatch("administration/getUsersList")
+  }
+
+
+  return {
+    getUsers,
+    data: computed(() => store.getters["administration/dataInfo"]),
+  }
+};
+
+export default useAdministration
