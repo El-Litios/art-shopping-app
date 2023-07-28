730e003815da235f5b9ec914cd8c9635fb1580b0
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-28 01:31:27 -0400

    import user table component

--- src/modules/administration/views/Administration.vue
+++ src/modules/administration/views/Administration.vue
@@ -1,6 +1,19 @@
-<template></template>
+<template>
+    <v-btn @click="getUsers"></v-btn>
+    <UsersTable :data="data"></UsersTable>
+</template>
     
 <script setup>
+import { onMounted } from 'vue'
+import UsersTable from './../components/UsersTable.vue'
+import useAdministration from '../composables/useAdministration'
+
+const {
+    getUsers,
+    data
+} = useAdministration();
+
+
 
 </script>
     
