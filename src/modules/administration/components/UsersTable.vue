ab1ad65922a1690306a0238f1fe9dab10efbe969
El-Litios <carlosguzmanrojas00@gmail.com>
2023-07-28 01:30:03 -0400

    add user table component

---
+++ src/modules/administration/components/UsersTable.vue
@@ -0,0 +1,20 @@
+<template>
+    <TableCore :items="data" :columns="userColumns">
+        <template v-slot:edit="{ item }">
+            <button @click="editUser(item)">Editar Usuario</button>
+        </template>
+        <template v-slot:delete="{ item }">
+            <button @click="deleteUser(item)">Eliminar Usuario</button>
+        </template>
+    </TableCore>
+</template>
+
+<script setup>
+import TableCore from './TableCore.vue'
+const props = defineProps(['data'])
+console.log(props.data)
+const userColumns = [
+    { key: "name", label: "Nombre" },
+    { key: "email", label: "Email" },
+]
+</script>
