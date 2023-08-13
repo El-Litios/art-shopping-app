fccb134f7b27c3fcc63c0d469c3c37ec9c10af5e
El-Litios <carlosguzmanrojas00@gmail.com>
2023-08-13 19:03:13 -0400

    Import button group component

--- src/modules/administration/views/Administration.vue
+++ src/modules/administration/views/Administration.vue
@@ -1,13 +1,16 @@
 <template>
     <Navbar></Navbar>
+    
     <v-btn @click="getUsers"></v-btn>
     <v-container>
+        <ButtonsGroup class="mb-5"></ButtonsGroup>
         <UsersTable :data="data"></UsersTable>
     </v-container>
 </template>
     
 <script setup>
 import UsersTable from './../components/UsersTable.vue'
+import ButtonsGroup from './../components/ButtonsGroup.vue'
 import Navbar from '@/components/shared/Navbar.vue'
 import useAdministration from '../composables/useAdministration'
 
