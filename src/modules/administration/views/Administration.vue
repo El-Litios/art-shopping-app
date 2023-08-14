52fa5c2f48521c0afa01bda4470fbc15850d7835
El-Litios <carlosguzmanrojas00@gmail.com>
2023-08-14 00:38:21 -0400

    Updated the data load function

--- src/modules/administration/views/Administration.vue
+++ src/modules/administration/views/Administration.vue
@@ -1,16 +1,19 @@
 <template>
     <Navbar></Navbar>
     <v-container>
-        <ButtonsGroup class="mb-5" @loadProducts="onLoadProducts"></ButtonsGroup>
+        <ButtonsGroup class="mb-5" @loadData="onLoadData"></ButtonsGroup>
         <UsersTable v-if="currentTable == 'Products'" :data="data"></UsersTable>
         <div v-if="!isLoaded" class="Message text-center text-h4 pt-5">
             Debes presionar un boton para cargar datos....
+        </div>
+        <div v-if="isLoading" class="Message text-center text-h4 pt-5">
+            Cargando datos....
         </div>
     </v-container>
 </template>
     
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import UsersTable from './../components/UsersTable.vue'
 import ButtonsGroup from './../components/ButtonsGroup.vue'
 import Navbar from '@/components/shared/Navbar.vue'
@@ -22,19 +25,28 @@ const {
 } = useAdministration();
 
 const currentTable = ref('');
-const isLoaded = ref(false)
+const isLoaded = ref(false);
+const isLoading = ref(false);
 
-function onLoadProducts(){
-    isLoaded.value = false
-    getUsers();
-    currentTable.value = 'Products'
+async function onLoadData(table) {
+    isLoading.value = true
+    switch (table) {
+        case 'Product':
+            await getUsers();
+            currentTable.value = 'Products'
+        break
+        case 'Category': 
+        console.log('Categoria');
+        break
+    }
     isLoaded.value = true
+    isLoading.value = false
 }
 
 </script>
     
 <style>
-.Message{
+.Message {
     background-color: rgba(27, 27, 27, 0.685) !important;
     backdrop-filter: blur(10px);
     height: 100px;
