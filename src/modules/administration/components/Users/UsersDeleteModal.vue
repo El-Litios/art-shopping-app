ac1b30fcaa6e9234c77f997ec926538f1d2aa695
El-Litios <carlosguzmanrojas00@gmail.com>
2023-10-11 00:35:56 -0300

    add delete a user modal

---
+++ src/modules/administration/components/Users/UsersDeleteModal.vue
@@ -0,0 +1,25 @@
+<template>
+    <Modal v-model="showDeleteModal">
+        <template v-slot:modal-button>
+            <v-icon class="mdi mdi-trash-can-outline"></v-icon>
+        </template>
+
+        <template v-slot:modal-title>
+            Eliminar Informacion de Usuario
+        </template>
+        <template v-slot:modal-save-button>
+            <v-btn rounded="lg" size="large" class="text-h5" color="red-darken-1" variant="tonal"
+                @click="$emit('unshowmodal')">>
+                <v-icon class="mdi mdi-window-close"></v-icon>
+            </v-btn>
+            <v-btn rounded="lg" size="large" class="text-h5" color="blue-darken-1" variant="tonal"
+                @click="$emit('unshowmodal')">>
+                <v-icon class="mdi mdi-content-save"></v-icon>
+            </v-btn>
+        </template>
+    </Modal>
+</template>
+
+<script setup>
+import Modal from '../Modal.vue'
+</script>
